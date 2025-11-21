c42c9b6aa0194d7e25769d5ef42a7527b96f38e5
Julieta Sorrentino <x24217115@student.ncirl.ie>
2025-11-21 19:16:37 +0000

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,16 +1,16 @@
 alert("ok");
 
 fetch("components/navbar.html")
-  .then(response => response.text())
-  .then(data => {
+    .then(response => response.text())
+    .then(data => {
       document.getElementById("navbarAdd").innerHTML = data;
-
-      document.querySelectorAll('.nav-link').forEach(link => {
-          if (link.href === window.location.href) {
-              link.classList.add('active');
-              link.setAttribute('aria-current', 'page');
-          }
-      });
-  });
+});
 
 // Styling menu buttons, coloring, effects when accessing page
+
+  document.querySelectorAll('.nav-link').forEach(link => {
+  if(link.href === window.location.href) {
+    link.classList.add('active');
+    link.setAttribute('aria-current','page');
+  }
+});
